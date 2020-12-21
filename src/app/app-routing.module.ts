e7cd84755c0818a43f1f22b36558c018a7d2aab6
ayman.eldemerdash <ayman.el-demerdash@outlook.com>
2020-12-22 00:39:32 +0200

    landing page done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/components/login/login.component';
+import { HomeComponent } from './main/components/home/home.component';
+import { DashboardComponent } from './main/components/dashboard/dashboard.component';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: '**', redirectTo: 'login' }
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ]
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
 ];
 
 @NgModule({
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { SharedModule } from '../shared/shared.module';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    DashboardComponent
+  ],
   imports: [
-    CommonModule
+    SharedModule
   ]
 })
 export class MainModule { }
